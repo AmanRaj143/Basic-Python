2846e8200d3c6a9eae4691d4c10ef74681254a18
Aman raj <110926344+AmanRaj143@users.noreply.github.com>
2022-08-10 18:41:48 +0530

    Update commaseprated.py

--- commaseprated.py
+++ commaseprated.py
@@ -1,3 +1,9 @@
+#Write a Python program which accepts a sequence of comma-separated numbers from the
+#user and generates a list and a tuple with those numbers.
+#Sample data : 3, 5, 7, 23
+#Output :
+#List : ['3', ' 5', ' 7', ' 23']
+#Tuple : ('3', ' 5', ' 7', ' 23')
 values = input("Input some comma seprated numbers :- ")
 list = values.split(",")
 tuple = tuple(list)
