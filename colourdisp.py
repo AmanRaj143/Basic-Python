d3f41d09e3f39e2d23f7591950355e2bc8d16651
Aman raj <110926344+AmanRaj143@users.noreply.github.com>
2022-08-10 18:43:46 +0530

    Update colourdisp.py

--- colourdisp.py
+++ colourdisp.py
@@ -1,2 +1,4 @@
+#Write a Python program to display the first and last colours from the following list.
+#color_list = ["Red","Green","White" ,"Black"]
 color_list = ["Red","Green","White" ,"Black"]
 print( "%s %s"%(color_list[0],color_list[-1]))
